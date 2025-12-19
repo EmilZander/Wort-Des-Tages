9c0b781d24f5c3891d8b14e1ef5215e447414e54
Emil <emil@e57.de>
2025-12-19 20:43:38 +0100

    Add error handling

--- wdt-venv/main.py
+++ wdt-venv/main.py
@@ -19,21 +19,27 @@ class HtmlScraper:
         self.url = url
     
     def html_get(self):
-        response = requests.get(self.url)
+        try:
+            response = requests.get(self.url)
+        except Exception as e:
+            raise Exception(f"Failed to fetch HTML: {e}]")
+
         return response.text
 
     def result_get(self):
-        if (len(self.seperators) == 0):
-            return self.html_get()
+        try:
+            if (len(self.seperators) == 0):
+                return self.html_get()
         
-        result = self.html_get()
+            result = self.html_get()
 
-        for seperator in self.seperators:
-            if seperator.seperator == "" or result.find(seperator.seperator) == -1:
-                continue
+            for seperator in self.seperators:
+                if seperator.seperator == "" or result.find(seperator.seperator) == -1:
+                    continue
 
             result = result.split(seperator.seperator)[0 if seperator.use_left_side else 1]
-        
+        except Exception as e:
+            raise Exception(f"Failed to paarse result: {e}]")
         return result
 
 class Config:
@@ -43,23 +49,28 @@ class Config:
     recipent_phonenumbers = []
 
     def base_url_get(self):
-        if (self.url.find("https://") == -1 and self.url.find("http://") == -1):
-            return self.url.split("/")[1]
+        try:
+            if (self.url.find("https://") == -1 and self.url.find("http://") == -1):
+                return self.url.split("/")[1]
+            return self.url.split("//")[1].split("/")[0] + "/"
+        except Exception as e:
+            raise Exception(f"Failed to fetch base URL: {e}]")
         
-        return self.url.split("//")[1].split("/")[0] + "/"
     
     def __init__(self, path):
         self.loadFromJson(path)
 
     def loadFromJson(self, path: str):
+        try:
+            with open(path, "r") as file:
+                data = json.load(file)
 
-        with open(path, "r") as file:
-            data = json.load(file)
-
-            self.url = data["url"]
-            self.enabled = data["enabled"]
-            self.sender_phonenumber = data["sender_phonenumber"]
-            self.recipent_phonenumbers = data["recipent_phonenumbers"]
+                self.url = data["url"]
+                self.enabled = data["enabled"]
+                self.sender_phonenumber = data["sender_phonenumber"]
+                self.recipent_phonenumbers = data["recipent_phonenumbers"]
+        except Exception as e:
+            raise Exception(f"Failed to load config from JSON: {e}]")
 
 class SignalMessenger:
     signalCLI_path = "signal-cli"
@@ -70,30 +81,35 @@ class SignalMessenger:
         self.sender_number = sender_number
 
     def is_registered(self) -> bool:
-        result = subprocess.run(
+        try:
+            result = subprocess.run(
             [self.signalCLI_path, "-u", self.sender_number, "check", self.sender_number],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True
-        )
-
-        output = result.stdout.strip()
-        error = result.stderr.strip()
-
-        if error:
-            print(f"Fehler: {error}")
-            return False
-
-        return "true" in output.lower()
-    
-    def send_message(self, recipient_numbers: list[str], message: str):
-        for recipient in recipient_numbers:
-            subprocess.run(
-                [self.signalCLI_path, "-u", self.sender_number, "send", recipient, "-m", message],
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
                 text=True
             )
+            output = result.stdout.strip()
+            error = result.stderr.strip()
+
+            if error:
+                print(f"Fehler: {error}")
+                return False
+
+            return "true" in output.lower()
+        except Exception as e:
+            raise Exception(f"Failed to check registration: {e}")
+    
+    def send_message(self, recipient_numbers: list[str], message: str):
+        try:
+            for recipient in recipient_numbers:
+                subprocess.run(
+                    [self.signalCLI_path, "-u", self.sender_number, "send", recipient, "-m", message],
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    text=True
+                )
+        except Exception as e:
+            raise Exception(f"Failed to send message: {e}")
 
 def main():
     config = Config("config.json")
